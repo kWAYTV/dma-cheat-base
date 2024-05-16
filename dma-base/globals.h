109242191b37903e770c5f1b61066825e4526f2c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-05-16 21:11:33 +0200

    Made easier editing for ProcName & ProcBase values for memory class initialization

--- dma-base/globals.h
+++ dma-base/globals.h
@@ -4,8 +4,9 @@
 #define DEBUG_INFO 1
 #define USE_LOCALHOST 0
 
-namespace globals {
-	inline bool IsRunning = true;
-	inline bool Localhost = true;
+namespace globals
+{
+	inline std::string ProcName = "cs2.exe";
+	inline std::string ProcBase = "client.dll";
 	inline uintptr_t ClientBase = NULL;
-}
+};
--- dma-base/main.cpp
+++ dma-base/main.cpp
@@ -4,13 +4,13 @@ bool main()
 {
     SetConsoleTitleA(skCrypt("Awhare - Developer Console"));
 
-	if (!Mem.Init("cs2.exe"))
+	if (!Mem.Init(globals::ProcName)
 	{
 		LOG(skCrypt("Failed to initialize process\n"));
 		return false;
 	}
 
-	globals::ClientBase = Mem.GetBaseAddress("client.dll");
+	globals::ClientBase = Mem.GetBaseAddress(globals::ProcBase);
 	Mem.FixCr3();
 
     system(skCrypt("pause"));
